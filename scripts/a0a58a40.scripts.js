14b3defd878a05779de99645328ca6665172f495
deploy.sh <XX1011+deploy.sh@gmail.com>
2013-09-10 03:09:13 +0000

    publish: enable deployment from Travis using HTTPS URL with encrypted OAuth token
    generated from commit 15f14caaee1831cc00340d777e7dfe56457dfa9e

---
+++ scripts/a0a58a40.scripts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+'use strict';
+
+function Bingo($scope) {
+	$scope.board = newBoard(5, 5);
+}
+
+function newBoard(rows, cols) {
+	var board = new Array(rows);
+	for (var i = 0; i < rows; i++) {
+		var row = board[i] = new Array(cols);
+		for (var j = 0; j < cols; j++)
+			row[j] = {};
+	}
+	return board;
+}
