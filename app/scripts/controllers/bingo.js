8eaa91a83bec67d32ab0d47cfb2742f044f01c75
X1011 <XX1011@gmail.com>
2013-09-19 21:51:29 +0000

    real phrases, yay!

--- app/scripts/controllers/bingo.js
+++ app/scripts/controllers/bingo.js
@@ -1,4 +1,5 @@
 'use strict';
+/*jshint quotmark: false*/
 
 function Bingo($scope) {
 	$scope.board = board(texts, 5, 5);
@@ -16,4 +17,28 @@ function board(texts, rows, cols) {
 }
 
 //todo: define this in a separate file
-var texts = ['1', '2'];
+var texts = [
+	'webOS / Pre / Palm',
+	'Companion Core',
+	'IR blaster',
+	'Fast and Furious',
+	'S Pen',
+	'crazy Samsung press conference',
+	'segue',
+	'Microsoft Kin',
+	"Chris's connection is bad",
+	'breaking news',
+	'Vlad pronounces things wrong',
+	'holding a phone up to the screen',
+	'3-man Vergecast',
+	'HTC HD2',
+	'talking about food',
+	'Bohn Zone',
+	'violent Droid brand',
+	'Samsung crazy',
+	'Chris knows way too much about some obscure topic',
+	'“We’ve got a lot to talk about”',
+	'PureView',
+	'Dieter spinning in his chair',
+	'Sprint bashing'
+];
